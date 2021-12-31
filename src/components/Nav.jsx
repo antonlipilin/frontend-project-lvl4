83e26d1d0c14bb5a2e8cda49687590573d510d6d
Anton Lipilin <antonlipilinwork@gmail.com>
2021-12-31 21:25:02 +0400

    Add a navigation component

---
+++ src/components/Nav.jsx
@@ -0,0 +1,15 @@
+import { Navbar, Container } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import React from 'react';
+
+const Nav = () => (
+  <Navbar bg="white" fixed="top" className="p-3 shadow">
+    <Container>
+      <Navbar.Brand>
+        <Link to="/" className="brand-link">MyChat</Link>
+      </Navbar.Brand>
+    </Container>
+  </Navbar>
+);
+
+export default Nav;
