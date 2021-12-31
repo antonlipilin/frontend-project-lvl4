dc4ef905f1262e2bcbbda6c11b1958e2d17fa9e0
Anton Lipilin <antonlipilinwork@gmail.com>
2021-12-31 15:42:08 +0400

    Rework with react-bootstrap

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,72 +1,81 @@
 import { useFormik } from 'formik';
-import { Link } from 'react-router-dom';
-import React from 'react';
-import * as yup from 'yup';
+import { Link, useNavigate } from 'react-router-dom';
+import {
+  Form, Button, Row, Col,
+} from 'react-bootstrap';
+import React, { useState, useRef, useEffect } from 'react';
+import axios from 'axios';
+import useAuth from '../hooks/index.jsx';
 
 const LoginForm = () => {
+  const [authFailed, setAuthFailed] = useState(false);
+  const auth = useAuth();
+  const navigate = useNavigate();
+  const inputRef = useRef();
+
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
+
   const formik = useFormik({
     initialValues: {
-      login: '',
+      username: '',
       password: '',
     },
-    validationSchema: yup.object({
-      login: yup.string()
-        .min(3, 'Минимальная длина логина - 3 символа')
-        .max(15, 'Максимальная длина логина - 15 символов')
-        .required('Обязательное поле'),
-      password: yup.string()
-        .min(8, 'Минимальная длина пароля - 8 символов')
-        .max(15, 'Максимальная длина пароля - 15 символов')
-        .matches(/[0-9]/, 'Пароль должен содержать как минимум одну цифру')
-        .required('Обязательное поле'),
-    }),
-    onSubmit: (values) => {
-      console.log(values, 'FORM VALUES !!!');
-      formik.resetForm();
+    onSubmit: async (values) => {
+      try {
+        const res = await axios.post('/api/v1/login', values);
+        localStorage.setItem('userId', JSON.stringify(res.data));
+        auth.logIn();
+        navigate('/');
+      } catch (err) {
+        if (err.isAxiosError && err.response.status === 401) {
+          setAuthFailed(true);
+          inputRef.current.select();
+          return;
+        }
+        throw err;
+      }
     },
   });
 
   const renderForm = () => (
-    <form onSubmit={formik.handleSubmit} className="mb-3">
-      <div className="form-group">
-        <label htmlFor="login" className="w-100">
-          Введите Логин
-          <input
-            type="text"
-            required
-            placeholder="Логин"
-            id="login"
-            value={formik.values.login}
-            onBlur={formik.handleBlur}
-            onChange={formik.handleChange}
-            className="form-control form-control-lg mt-2"
-          />
-        </label>
-        {formik.touched.login && formik.errors.login ? <div style={{ color: 'red' }}>{formik.errors.login}</div> : null}
-      </div>
-      <div className="form-group">
-        <label htmlFor="password" className="w-100">
-          Введите Пароль
-          <input
-            type="password"
-            required
-            placeholder="Пароль"
-            id="password"
-            value={formik.values.password}
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            className="form-control form-control-lg mt-2"
-          />
-        </label>
-        {formik.touched.password && formik.errors.password ? <div style={{ color: 'red' }}>{formik.errors.password}</div> : null}
-      </div>
-      <button className="btn btn-outline-primary w-100" type="submit">Войти</button>
-    </form>
+    <Form onSubmit={formik.handleSubmit} className="mb-3">
+      <Form.Group controlId="username" className="mb-3">
+        <Form.Label>Введите логин</Form.Label>
+        <Form.Control
+          size="lg"
+          type="text"
+          required
+          ref={inputRef}
+          value={formik.values.username}
+          placeholder="Логин"
+          onBlur={formik.handleBlur}
+          onChange={formik.handleChange}
+          isInvalid={authFailed}
+        />
+      </Form.Group>
+      <Form.Group controlId="password" className="mb-3">
+        <Form.Label>Введите пароль</Form.Label>
+        <Form.Control
+          size="lg"
+          type="password"
+          placeholder="Пароль"
+          required
+          value={formik.values.password}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          isInvalid={authFailed}
+        />
+        <Form.Control.Feedback type="invalid">Неверный логин или пароль. Проверьте правильность введёных данных</Form.Control.Feedback>
+      </Form.Group>
+      <Button variant="outline-primary" className="w-100" type="submit">Войти</Button>
+    </Form>
   );
 
   return (
-    <div className="h-100 row justify-content-center align-items-center">
-      <div className="col-sm-10 col-md-8 col-xl-6 p-5 border rounded bg-white shadow">
+    <Row className="h-100 justify-content-center align-items-center">
+      <Col sm="10" md="8" xl="6" className="p-5 border rounded bg-white shadow">
         <h1 className="text-center">Войти</h1>
         {renderForm()}
         <div className="text-center">
@@ -74,8 +83,8 @@ const LoginForm = () => {
           {' '}
           <Link to="/signup">Регистрация</Link>
         </div>
-      </div>
-    </div>
+      </Col>
+    </Row>
   );
 };
 
--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Col, Row } from 'react-bootstrap';
 
 const NotFound = () => (
-  <div className="h-100 row align-content-center justify-content-center">
-    <div className="col-12 text-center">
+  <Row className="h-100 align-content-center justify-content-center">
+    <Col xs="12" className="text-center">
       <Link to="/" className="btn btn-outline-primary">Вернуться на главную</Link>
-    </div>
-    <div className="col-10 not-found h-50 text-center" />
-  </div>
+    </Col>
+    <Col xs="10" className="not-found h-50 text-center" />
+  </Row>
 );
 
 export default NotFound;
