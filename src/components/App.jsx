5df62f1c9e2456b962dfdccba5878d4d2d031e2a
Anton Lipilin <antonlipilinwork@gmail.com>
2021-12-31 16:42:27 +0400

    Add a local storage check on the main page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,7 +9,7 @@ import NotFound from './NotFound.jsx';
 import Chat from './Chat.jsx';
 
 const AuthProvider = ({ children }) => {
-  const [loggedIn, setLoggedIn] = useState(false);
+  const [loggedIn, setLoggedIn] = useState(() => (!!localStorage.getItem('userId')));
   const logIn = () => setLoggedIn(true);
   const logOut = () => {
     localStorage.removeItem('userId');
