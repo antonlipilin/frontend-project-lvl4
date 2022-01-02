0ed1aa4a3e8ca05748628e08d31b286df4009f29
Anton Lipilin <antonlipilinwork@gmail.com>
2022-01-02 18:26:14 +0400

    Add logoutButton to  nav bar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,6 +2,7 @@ import { Navbar, Container } from 'react-bootstrap';
 import '../../assets/Nav.scss';
 import { Link } from 'react-router-dom';
 import React from 'react';
+import LogoutButton from './LogoutButton.jsx';
 
 const Nav = () => (
   <Navbar bg="white" fixed="top" className="p-3 shadow">
@@ -9,6 +10,7 @@ const Nav = () => (
       <Navbar.Brand>
         <Link to="/" className="brand-link">MyChat</Link>
       </Navbar.Brand>
+      <LogoutButton />
     </Container>
   </Navbar>
 );
