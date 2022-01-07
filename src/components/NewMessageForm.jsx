cf7e5bee18ea3b831067d29d90f097048ad9ee93
Anton Lipilin <antonlipilinwork@gmail.com>
2022-01-07 14:47:46 +0400

    Add sending messages via socket

--- src/components/NewMessageForm.jsx
+++ src/components/NewMessageForm.jsx
@@ -1,23 +1,39 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 import { useFormik } from 'formik';
 import { Form, InputGroup, Button } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
+import { SocketContext } from '../contexts/socket.jsx';
+import { addMessage } from '../slices/messagesSlice.js';
 
 const NewMessageForm = () => {
   const currentChannelId = useSelector((state) => state.channels.currentChannelId);
   const dispatch = useDispatch();
+  const socket = useContext(SocketContext);
+
+  useEffect(() => {
+    socket.on('newMessage', (msg) => {
+      console.log(msg);
+      dispatch(addMessage(msg));
+    });
+  }, []);
 
   const formik = useFormik({
     initialValues: {
       text: '',
     },
     onSubmit: (values) => {
-      console.log(values);
+      const { username } = JSON.parse(localStorage.getItem('userId'));
+      const { text } = values;
+      const message = { text, username, channelId: currentChannelId };
+      socket.emit('newMessage', message, (response) => {
+        console.log(response.status);
+      });
+      formik.resetForm();
     },
   });
 
   return (
-    <div className="mt-auto p-2">
+    <div className="mt-auto py-3 px-2">
       <Form onSubmit={formik.handleSubmit}>
         <InputGroup>
           <Form.Control
