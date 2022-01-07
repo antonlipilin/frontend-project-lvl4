28bf70c5b5be832dcc07b297060bc82ccfd1a5df
Anton Lipilin <antonlipilinwork@gmail.com>
2022-01-07 14:24:53 +0400

    Add socket context

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 import { Container, Row } from 'react-bootstrap';
 import Channels from './Channels.jsx';
 import Messages from './Messages.jsx';
+import { socket, SocketContext } from '../contexts/socket.jsx';
 
 const Chat = () => (
-  <Container className="my-4 h-100 shadow rounded bg-white">
+  <Container className="my-4 h-100 shadow rounded bg-white overflow-hidden">
     <Row className="h-100">
-      <Channels />
-      <Messages />
+      <SocketContext.Provider value={socket}>
+        <Channels />
+        <Messages />
+      </SocketContext.Provider>
     </Row>
   </Container>
 );
