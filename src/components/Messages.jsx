5a2a6bb4b58e3794da913896ef24f2d5a891f397
Anton Lipilin <antonlipilinwork@gmail.com>
2022-01-07 14:35:23 +0400

    Add function for rendering messages

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -9,17 +9,32 @@ const Messages = () => {
   const currentChannelId = useSelector((state) => state.channels.currentChannelId);
   const messagesByChannelId = messages.filter((message) => message.channelId === currentChannelId);
 
-  const renderMessages = () => null;
+  const renderMessages = () => {
+    if (messagesByChannelId.length === 0) {
+      return null;
+    }
+
+    return messagesByChannelId.map((message) => (
+      <div key={message.id} className="mb-2 text-break">
+        <b>
+          {message.username}
+          :
+        </b>
+        {' '}
+        {message.text}
+      </div>
+    ));
+  };
 
   return (
-    <Col className="p-0 d-flex flex-column">
+    <Col className="p-0 d-flex flex-column h-100">
       <div className="small p-3 mb-3 bg-light border-bottom">
         <p className="mb-0">
           <b># Имя чата</b>
         </p>
         <span>Количество сообщений</span>
       </div>
-      <div id="messages-box" className="chat-messages overflow-auto px-5 ">
+      <div id="messages-box" className="chat-messages overflow-auto px-5">
         {renderMessages()}
       </div>
       <NewMessageForm />
