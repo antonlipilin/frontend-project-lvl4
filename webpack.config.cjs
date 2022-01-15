1eb8709b5fd0ce59f5dc5ea878943dc10bcc63c2
Anton Lipilin <antonlipilinwork@gmail.com>
2022-01-15 14:04:12 +0400

    Add css loader

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -44,6 +44,10 @@ module.exports = {
           { loader: 'sass-loader' },
         ],
       },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
     ],
   },
 };
